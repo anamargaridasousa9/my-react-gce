625a095f6ed7e5ab7a8be22cc3439274007b601c
anamargaridasousa9 <ana.margarida.d.sousa@gmail.com>
2022-07-29 20:49:15 +0100

    Add styles to custom popup

--- src/components/popup/index.js
+++ src/components/popup/index.js
@@ -1,33 +1,17 @@
+import Header from "./Header";
+import Body from "./Body";
+import Footer from "./Footer";
 import { playlist } from "./values";
+import "./styles.css";
 
-function FunPopup() {
+const FunPopup = () => {
   return (
-    <div>
-      <div id="fun-header">
-        <h1>My youtube playlist</h1>
-      </div>
-      <div id="fun-body">
-        <ul>
-          {playlist.map((video, index) => (
-            <li key={index}>
-              <a href={video.url} target="_blank" rel="noreferrer">
-                {video.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div id="fun-footer">
-        <a
-          href="https://www.notion.so/React-Google-Chrome-Extension-for-beginners-or-not-Onboarding-92da8245f8234e1bb44abf7a6e90405c"
-          target="_blank"
-          rel="noreferrer"
-        >
-          React GCE Tutorial 2021
-        </a>
-      </div>
+    <div className="popup__font">
+      <Header />
+      <Body playlist={playlist} />
+      <Footer />
     </div>
   );
-}
+};
 
 export default FunPopup;
